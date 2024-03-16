26804d982efeb54520d9d05a387749cb2b0bf39e
Samxiety <156465125+Samxiety@users.noreply.github.com>
2024-03-16 14:37:47 +0100

    Count round outcome and alert game outcome

--- script.js
+++ script.js
@@ -8,27 +8,52 @@
     return compChoice[randomPick];
  }
 // Declare a message variable for every outcome
-const win = ("You win this round!")
-const lose = ("You lose this round!")
-const tie = ("That's a tie!")
-const falseInput = ("Wtf?? You have to pick Rock, Paper or Scissors to play!")
+const winRound = "You win this round!"
+const loseRound = "You lose this round!"
+const tieRound = "That's a tie!"
+const falseInput = "?? You have to pick Rock, Paper or Scissors to play!"
+const winGame = "Congratulations, you win the game!"
+const loseGame = "I'm sorry, but you lost this game :("
+const tieGame = "It looks like the game is a tie!"
 
-/* Create a function playGame() that asks if you want to play the game
+/* 
+   Create a function playGame() that asks if you want to play the game
    if ok start playRound, else cancel and alert "You don't want to play? Ok, bye"
    Use a while loop.
+   Count the result of each game and console.log it. 
+   If cancel game console.log game result.
 */
 
 function playGame() {
    let playAgainLoop = true;
+   let countResult = 0;
 
    while (playAgainLoop === true) {
       let askForPlay = "Do you want to play a round of Rock, Paper, Scissors?"
       let cancelGame = "You don't want to play? Bye then :)"
       if (confirm(askForPlay)) {
-         playRound()
+         let outcome = playRound()
+         if (outcome === winRound) {
+            countResult += 1;
+            console.log("win")
+         } else if (outcome === loseRound) {
+            countResult -= 1;
+            console.log("lose");
+         } else if (outcome === tieRound) {
+            console.log("tie");
+         } else if (outcome === falseInput) {
+            console.log(" - ");
+         }
       } else {
          alert(cancelGame);
          playAgainLoop = false;
+         if (countResult > 0) {
+            console.log(winGame);
+         } else if (countResult < 0) {
+            console.log(loseGame)
+         } else {
+            console.log(tieGame)
+         }
       }
 }
 }
@@ -40,15 +65,18 @@ function playRound() {
    alert("The computer picked " + compSelect)
    // check for a tie and alert if so
    if (playerSelect === compSelect) {
-     alert(tie)
+      alert(tieRound)
+      return tieRound
    }  
    
    // run if player selects rock
    else if (playerSelect === "rock") {
         if (compSelect === "scissors") {
-           alert(win)
+            alert(winRound)
+            return winRound
         } else if (compSelect === "paper") {
-           alert(lose)
+            alert(loseRound)
+            return loseRound
         } 
    } 
 
@@ -56,11 +84,11 @@ function playRound() {
    else if (playerSelect === "scissors") {
       switch (compSelect) {
          case "paper":
-            alert(win)
-            break
+            alert(winRound)
+            return winRound 
          case "rock":
-            alert(lose)
-            break
+            alert(loseRound)
+            return loseRound
       }
    }
 
@@ -68,11 +96,11 @@ function playRound() {
    else if (playerSelect === "paper") {
       switch (compSelect) {
          case "rock":
-            alert(win)
-            break
+            alert(winRound)
+            return winRound
          case "scissors":
-            alert(lose)
-            break
+            alert(loseRound)
+            return loseRound
       }
    }
    // run if player gives an invalid input
